d15e26dc5f7c9003d9d4da21ee0b08580cb1b32d
eyüp ertan <eyupsagdis39@gmail.com>
2021-11-22 00:07:28 +0300

    testtt

--- polls/models.py
+++ polls/models.py
@@ -9,7 +9,7 @@ from polls.views import validate_even, validatePhone,validateHesCode
 
 # Create your models here.
 
-class Donem(models.Model):# deneme2
+class Donem(models.Model):# deneme2 olmadııı 
     yillar=models.TextField(max_length=10,default="")
     
     def __str__(self): 
