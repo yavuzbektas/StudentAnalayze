d94cb8d65ce553d63442a2204d053138783888e4
eyüp ertan <eyupsagdis39@gmail.com>
2021-11-24 17:34:22 +0300

    main

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,15 +4,18 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path, re_path
-from apps.home import views
+from apps.home.views import login_view, register_user
+from apps.home import LogoutView, views
 from apps.polls import views
 
 urlpatterns = [
+    path('login/', login_view, name="login"),
+    path('register/', register_user, name="register"),
   
     # The home page
     path('', views.index, name='home'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
-
+    path("logout/", LogoutView.as_view(), name="logout")
 ]
