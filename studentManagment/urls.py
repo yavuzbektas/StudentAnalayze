809ebfba9219c0621eda886fd2651c3e20c65c2e
eyüp ertan <eyupsagdis39@gmail.com>
2021-11-24 21:45:12 +0300

    main

--- studentManagment/urls.py
+++ studentManagment/urls.py
@@ -26,10 +26,10 @@ from django.conf.urls.static import static
 urlpatterns = [
  
     path("", include("apps.polls.urls")),
-    path("account/", include("account.urls")),
+    path("account/", include('account.urls')),
     path('admin/', admin.site.urls),          # Django admin route
    # path("", include("apps.authentication.urls")), # Auth routes - login / register
     path("", include("apps.home.urls"))             # UI Kits Html files
          # UI Kits Html files
-]#+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] #+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
